28c414b0bdda33b8b795d035d5181b75a30593c3
carlosmantilla <carlosmantilla@localhost>
2021-02-17 22:18:46 -0500

    Commit inicial con STS

---
+++ Login2021/src/test/java/com/carlosmantilla/login2021/Login2021ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carlosmantilla.login2021;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Login2021ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
