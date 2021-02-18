b88e4b4637b19db57fc107c0754b63047026635c
carlosmantilla <carlosmantilla@localhost>
2021-02-17 22:18:46 -0500

    Commit inicial con STS Close #1

---
+++ Login2021/src/main/java/com/carlosmantilla/login2021/Login2021Application.java
@@ -0,0 +1,13 @@
+package com.carlosmantilla.login2021;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Login2021Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Login2021Application.class, args);
+	}
+
+}
